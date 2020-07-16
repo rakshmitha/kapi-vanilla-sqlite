08bed404ba7fc26d684527929588e329277156ce
Vaishnavi <vaisnu200531@yahoo.com>
2020-07-16 21:16:12 +0530

    first commit from flask grp

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, jsonify, request, make_response
 import sqlite3
 import zenv
 from sqlite3 import Error
-from bubble_crud import *
+import bubble_crud as bc
+import insert as ins
 database = zenv.DB_LOCATION
 
 
@@ -17,8 +18,66 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
-    result = select_all(get_db())
+    result = bc.select_all(get_db())
     return make_response(jsonify(result), 200)
+
+'''
+    http://127.0.0.1:5001/select-all-by-actor/<actor_name>
+'''
+
+@app.route("/select-all-by-actor/<actor_name>")
+def select_all_by_actor(actor_name):
+        result = bc.select_all_by_actor(get_db(),actor_name)
+        return make_response(jsonify(result), 200)
+
+'''
+    http://127.0.0.1:5001/add-coartist-bubble
+'''
+@app.route("/add-coartist-bubble")
+def add_coartist_bubble():
+        obj=request.get_json(force=True)
+        print("JSON INPUT ::::  \n",obj)
+        result = bc.add_coartist_bubble(get_db(),obj)
+        return make_response(jsonify(result), 200)
+
+'''
+    http://127.0.0.1:5001/update-movie
+'''
+@app.route("/update-movie")
+def update_movie():
+        obj=request.get_json(force=True)
+        print("JSON INPUT ::::  \n",obj)
+        result = bc.update_movie(get_db(),obj)
+        return make_response(jsonify(result), 200)
+
+
+
+
+'''
+http://127.0.0.1:5001/delete-movie/<name>
+'''
+@app.route("/delete-movie/<name>")
+def delete_movie(name):
+    result = bc.delete_movie(get_db(),name)
+    return make_response(jsonify(result), 200)
+
+
+'''
+http://127.0.0.1:5001/delete-all-cities
+'''
+@app.route("/delete-all-cities")
+def delete_all_cities():
+    result = bc.delete_all_cities(get_db())
+    return make_response(jsonify(result), 200)
+       
+'''
+http://127.0.0.1:5001/add-movie/<name>/<releasedate>/<starring>
+'''
+@app.route("/add-movie/<name>/<releasedate>/<starring>")
+def add_movie(name,releasedate,starring):
+    result = ins.add_movie(name,releasedate,starring)
+    return make_response(jsonify(result), 200)
+
 
 
 # def select_all():
